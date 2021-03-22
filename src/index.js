d1eb1d2b9f2ca8d3e5a7f8236d3455110b9879b9
MaxDemydenko <max.demydenko.contact@gmail.com>
2021-03-22 10:04:16 +0200

    test commit

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import {App} from './App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from "react-redux";
 import store from "./redux/store";
+import {HashRouter} from "react-router-dom";
 
 ReactDOM.render(
     <React.StrictMode>
-        <Provider store={store}>
-            <App/>
-        </Provider>
+        <HashRouter>
+            <Provider store={store}>
+                <App/>
+            </Provider>
+        </HashRouter>
     </React.StrictMode>,
     document.getElementById('root')
 );
